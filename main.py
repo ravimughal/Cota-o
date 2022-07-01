c1b237f292d8080080594fee99b895c91909c517
Ravi Mughal <ravimughal@hotmail.com>
2022-06-30 23:32:34 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,23 +2,16 @@
 from time import sleep
 import requests
 
-DOL = "https://economia.awesomeapi.com.br/last/USD-BRL"
-EUR = "https://economia.awesomeapi.com.br/last/EUR-BRL"
-BTC = "https://economia.awesomeapi.com.br/last/BTC-BRL"
+req = requests.get("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL").json()
 
-dam = requests.get(DOL).json()
-dam_USD = dam["USDBRL"]
-
-eam = requests.get(EUR).json()
-eam_EUR = eam["EURBRL"]
-
-bam = requests.get(BTC).json()
-bam_BTC = bam["BTCBRL"]
+usd = req["USDBRL"]["bid"]
+eur = req["EURBRL"]["bid"]
+btc = req["BTCBRL"]["bid"]
 
 while True:
     print("-" * 30)
-    print("Bitcoin:",bam_BTC["bid"])
-    print("Euro:",eam_EUR["bid"])
-    print("Dólar:",dam_USD["bid"])
+    print("Bitcoin:", btc)
+    print("Euro:", eur)
+    print("Dólar:", usd)
     print("-" * 30)
     sleep(30)
